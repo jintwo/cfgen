9b7926a41001d10ab213133f9157bfb70896719b
Eugeny Volobuev <qulert@gmail.com>
2013-05-29 02:10:46 +0400

    initial commit

---
+++ cfgen/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import argparse
+import json
+from os import path
+
+from jinja2 import Environment, FileSystemLoader
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('settings', help='Settings file')
+    parser.add_argument('profile', help='Profile name')
+    parser.add_argument('-t', '--templates', help='Templates root', default='.')
+    parser.add_argument('-o', '--output', help='Output path', default='.')
+    args = parser.parse_args()
+    env = Environment(loader=FileSystemLoader(args.templates))
+    config = json.loads(open(args.settings, 'r').read())
+
+    for template_name, data in config.items():
+        template = env.get_template(template_name)
+
+        output_file_name = data.get('output')
+        if not output_file_name:
+            raise Exception('Invalid output file name.')
+
+        profiles = data.get('profiles', {}).copy()
+        if args.profile not in profiles:
+            raise Exception('Invalid profile name')
+
+        template_params = data.get('defaults', {}).copy()
+        template_params.update(profiles[args.profile])
+
+        output_data = template.render(**template_params)
+        with open(path.join(args.output, output_file_name), 'w') as output_file:
+            output_file.write(output_data)
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import cfgen
+
+setup(
+    name='CFGen',
+    version=cfgen.__VERSION__,
+    install_requires=['Jinja2>=2.6'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cfgen = cfgen.main:main'
+        ]
+    }
+)
