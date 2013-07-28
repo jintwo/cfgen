22cb1f1d9d0b4a17b692f3bbf6cccced79e5767f
Eugeny Volobuev <qulert@gmail.com>
2013-07-28 16:39:17 +0400

    minor improvements
    $include(/path/to/file.json) directive
    $(ENV_VAR) expression

--- cfgen/main.py
+++ cfgen/main.py
@@ -3,9 +3,38 @@
 from __future__ import print_function
 import argparse
 import json
-from os import path
+from os import path, getenv
+import re
 
 from jinja2 import Environment, FileSystemLoader
+
+def walk(dict_, fn=lambda value: value):
+    result = {}
+    for key, value in dict_.iteritems():
+        if isinstance(value, dict):
+            result[key] = walk(value, fn)
+        else:
+            result[key] = fn(value)
+    return result
+
+def env(value):
+    if not isinstance(value, basestring):
+        return value
+    result = value
+    matches = re.findall(r'\$\(.*?\)', result)
+    if matches:
+        for m in matches:
+            result = result.replace(m, getenv(m[2:-1], ''))
+    return result
+
+def include(value):
+    if not isinstance(value, basestring):
+        return value
+    result = value
+    if value.startswith('$include(') and value.endswith(')'):
+        filename = result.replace('$include(', '')[:-1]
+        result = json.loads(open(filename, 'r').read())
+    return result
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,12 +43,12 @@ def main():
     parser.add_argument('-t', '--templates', help='Templates root', default='.')
     parser.add_argument('-o', '--output', help='Output path', default='.')
     args = parser.parse_args()
-    env = Environment(loader=FileSystemLoader(args.templates))
+    jinja_env = Environment(loader=FileSystemLoader(args.templates))
     config = json.loads(open(args.settings, 'r').read())
     defaults = config.pop('defaults', {})
 
     for template_name, data in config.items():
-        template = env.get_template(template_name)
+        template = jinja_env.get_template(template_name)
 
         output_file_name = data.get('output')
         if not output_file_name:
@@ -34,7 +63,7 @@ def main():
             data.get('defaults', {}).items()
         )
         template_params.update(profiles[args.profile])
-
+        template_params = walk(template_params, lambda val: env(include(val)))
         output_data = template.render(**template_params)
         with open(path.join(args.output, output_file_name), 'w') as output_file:
             output_file.write(output_data)
