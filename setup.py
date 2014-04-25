d561ba5fcd9bd5f51b11e0d302a894fedca24e4a
Eugeny Volobuev <qulert@gmail.com>
2014-04-25 13:48:18 +0400

    setup fix

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-import cfgen
+
+__VERSION__ = (0, 2, 8)
 
 setup(
     name='CFGen',
-    version='.'.join(map(str, cfgen.__VERSION__)),
+    version='.'.join(map(str, __VERSION__)),
     install_requires=['Jinja2>=2.6'],
     packages=find_packages(),
     entry_points={
