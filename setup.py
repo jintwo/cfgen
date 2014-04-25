93e45326d4cb95651f148ef4c9ffdbd77d3124b4
Eugeny Volobuev <qulert@gmail.com>
2014-04-25 14:38:06 +0400

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
 __VERSION__ = (0, 2, 8)
 
 setup(
     name='CFGen',
+    author='Eugeny Volobuev',
+    author_email='qulert@gmail.com',
     version='.'.join(map(str, __VERSION__)),
+    url='http://github.com/jintwo/cfgen',
     install_requires=['Jinja2>=2.6'],
-    packages=find_packages(),
+    py_modules=['cfgen'],
     entry_points={
         'console_scripts': [
             'cfgen = cfgen:main'
